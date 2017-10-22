af30fdb0255e273e5ab82e85972e9ff462ca1092
Glo Alba <glomontalba7@gmail.com>
2017-10-22 23:38:14 +0200

    Add files via upload

---
+++ Letter_Of_Alphabet .c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main(){
+   
+    
+    char c;
+    
+    
+    scanf("%c", &c);
+    
+    if ((c >= 'A' && c <= 'Z') || ( c >= 'a' && c <= 'z'))
+        
+        printf("YES\n");
+    
+    else 
+        
+        printf("NO\n");
+    
+     
+    return 0;
+        
+        
+    
+    
+    
+    
+    
+    
+}
---
+++ vovel_VOVEL_consonant.c
@@ -0,0 +1,229 @@
+#include <stdio.h>
+
+
+int main(){
+    
+    
+    char char1, space, char2, space2, char7, space7;
+    
+    scanf("%c%c", &char1, &space);
+    
+    
+    switch (char1) {
+            
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            
+            printf("VOWEL\n");
+            break;
+            
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            
+            printf("vowel\n");
+            break;
+            
+        case 'b':
+        case 'B':
+        case 'c':
+        case 'C':
+        case 'd':
+        case 'D':
+        case 'f':
+        case 'F':
+        case 'g':
+        case 'G':
+        case 'h':
+        case 'H':
+        case 'j':
+        case 'J':
+        case 'k':
+        case 'K':
+        case 'l':
+        case 'L':
+        case 'm':
+        case 'M':
+        case 'n':
+        case 'N':
+        case 'p':
+        case 'P':
+        case 'q':
+        case 'Q':
+        case 'r':
+        case 'R':
+        case 's':
+        case 'S':
+        case 't':
+        case 'T':
+        case 'v':
+        case 'V':
+        case 'W':
+        case 'w':
+        case 'x':
+        case 'X':
+        case 'y':
+        case 'Y':
+        case 'z':
+        case 'Z':
+            
+            
+            printf("consonant\n");
+            break;
+            
+    }
+    
+    scanf("%c%c", &char2, &space2);
+    
+    switch (char2) {
+            
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            
+            printf("VOWEL\n");
+            break;
+            
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            
+            printf("vowel\n");
+            break;
+            
+        case 'b':
+        case 'B':
+        case 'c':
+        case 'C':
+        case 'd':
+        case 'D':
+        case 'f':
+        case 'F':
+        case 'g':
+        case 'G':
+        case 'h':
+        case 'H':
+        case 'j':
+        case 'J':
+        case 'k':
+        case 'K':
+        case 'l':
+        case 'L':
+        case 'm':
+        case 'M':
+        case 'n':
+        case 'N':
+        case 'p':
+        case 'P':
+        case 'q':
+        case 'Q':
+        case 'r':
+        case 'R':
+        case 's':
+        case 'S':
+        case 't':
+        case 'T':
+        case 'v':
+        case 'V':
+        case 'W':
+        case 'w':
+        case 'x':
+        case 'X':
+        case 'y':
+        case 'Y':
+        case 'z':
+        case 'Z':
+            
+            printf("consonant\n");
+            break;
+    }
+    
+    
+    scanf("%c%c", &char7, &space7);
+    
+    switch (char7) {
+            
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            
+            printf("VOWEL\n");
+            break;
+            
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            
+            printf("vowel\n");
+            break;
+            
+        case 'b':
+        case 'B':
+        case 'c':
+        case 'C':
+        case 'd':
+        case 'D':
+        case 'f':
+        case 'F':
+        case 'g':
+        case 'G':
+        case 'h':
+        case 'H':
+        case 'j':
+        case 'J':
+        case 'k':
+        case 'K':
+        case 'l':
+        case 'L':
+        case 'm':
+        case 'M':
+        case 'n':
+        case 'N':
+        case 'p':
+        case 'P':
+        case 'q':
+        case 'Q':
+        case 'r':
+        case 'R':
+        case 's':
+        case 'S':
+        case 't':
+        case 'T':
+        case 'v':
+        case 'V':
+        case 'W':
+        case 'w':
+        case 'x':
+        case 'X':
+        case 'y':
+        case 'Y':
+        case 'z':
+        case 'Z':
+            
+            printf("consonant\n");
+            break;
+            
+            
+            
+    }
+    
+    
+    
+    
+    return 0;
+    
+    
+}
